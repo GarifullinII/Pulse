37b6f03a18702a6af39fc28819317229502b49b0
GarifullinII <ildaridrisovich@gmail.com>
2021-01-02 23:27:07 +0400

    добавил carousel slick

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        dots: true,
+        speed: 1200,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.svg"></button>',
+        responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        dots: true,
+                        arrows: false
+                    }
+                }
+            ]
+        });
+    });
